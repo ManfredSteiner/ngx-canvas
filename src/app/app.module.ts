976803ef995141e26d275de6eaacd88acfeddeaf
Manfred Steiner <sx@htl-kaindorf.at>
2017-12-08 16:55:13 +0100

    step1

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, LogoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
