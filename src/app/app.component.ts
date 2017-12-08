ec908ad65ad83c52297a73d5ea6b6b54c504cf1b
Manfred Steiner <sx@htl-kaindorf.at>
2017-12-08 17:39:32 +0100

    step2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IParticle } from './logo.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    public title = 'step2';
+    public buttonText = 'Start Changes';
+    public particles: IParticle [] = [];
+
+    private _timer: any;
+
+    constructor () {
+        for (let i = 0 ; i < 50000 ; i++) {
+            this.particles.push({ x: Math.random() * 500, y: Math.random() * 500});
+        }
+    }
+
+    public onChanges () {
+        if (this._timer) {
+            clearInterval(this._timer);
+            this._timer = undefined;
+            this.buttonText = 'Start Changes';
+        } else {
+            this._timer = <any>setInterval(() => this.change(), 100);
+            this.buttonText = 'Stop Changes';
+        }
+    }
+
+    private change () {
+        const p: IParticle [] = [];
+        for (let i = 0 ; i < 50000 ; i++) {
+            p.push({ x: Math.random() * 500, y: Math.random() * 500});
+        }
+        this.particles = p;
+    }
+
 }
--- src/app/logo.component.ts
+++ src/app/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-logo',
@@ -6,13 +6,14 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 
 
-export class LogoComponent implements OnInit {
+export class LogoComponent implements OnInit, OnChanges {
 
+    @Input() particles: IParticle[];
     @ViewChild('myCanvas') canvasRef: ElementRef;
 
     ngOnInit() {
         const ctx: CanvasRenderingContext2D =
-          this.canvasRef.nativeElement.getContext('2d');
+            this.canvasRef.nativeElement.getContext('2d');
 
         // Draw the clip path that will mask everything else
         // that we'll draw later.
@@ -40,12 +41,57 @@ export class LogoComponent implements OnInit {
         // Draw 50,000 circles at random points
         ctx.beginPath();
         ctx.fillStyle = '#DD0031';
-        for (let i = 0 ; i < 50000 ; i++) {
-          const x = Math.random() * 500;
-          const y = Math.random() * 500;
-          ctx.moveTo(x, y);
-          ctx.arc(x, y, 1, 0, Math.PI * 2);
+        for (const {x, y} of this.particles) {
+            ctx.moveTo(x, y);
+            ctx.rect(x, y, 1, 1);
+        }
+        ctx.fill();
+    }
+
+    public ngOnChanges() {
+        const ctx: CanvasRenderingContext2D =
+            this.canvasRef.nativeElement.getContext('2d');
+
+        // Clear any previous content.
+        ctx.clearRect(0, 0, 500, 500);
+
+        // Draw the clip path that will mask everything else
+        // that we'll draw later.
+        ctx.beginPath();
+        ctx.moveTo(250, 60);
+        ctx.lineTo(63.8, 126.4);
+        ctx.lineTo(92.2, 372.6);
+        ctx.lineTo(250, 460);
+        ctx.lineTo(407.8, 372.6);
+        ctx.lineTo(436.2, 126.4);
+        ctx.moveTo(250, 104.2);
+        ctx.lineTo(133.6, 365.2);
+        ctx.lineTo(177, 365.2);
+        ctx.lineTo(200.4, 306.8);
+        ctx.lineTo(299.2, 306.8);
+        ctx.lineTo(325.2, 365.2);
+        ctx.lineTo(362.6, 365.2);
+        ctx.lineTo(250, 104.2);
+        ctx.moveTo(304, 270.8);
+        ctx.lineTo(216, 270.8);
+        ctx.lineTo(250, 189);
+        ctx.lineTo(284, 270.8);
+        ctx.clip('evenodd');
+
+        // Draw the points given as input
+        ctx.beginPath();
+        ctx.fillStyle = '#DD0031';
+        for (const {x, y} of this.particles) {
+            ctx.moveTo(x, y);
+            ctx.rect(x, y, 1, 1);
         }
         ctx.fill();
     }
 }
+
+export interface IParticle {
+    x: number;
+    y: number;
+}
+
+
